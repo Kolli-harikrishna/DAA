ed79b7bdc6d563d978b1c17626cee998df86408e
Kolli-harikrishna <kolliharikrishna249@gmail.com>
2025-11-26 12:54:12 +0530

    Add files via upload

---
+++ 7.base_function.py
@@ -0,0 +1,14 @@
+
+# its for the -ve and +ve powers
+def power(base,exponent):
+    result=1
+    for i in range(exponent):
+       result=result*base
+    return result
+
+base=2
+exponent=5
+res=power(base,abs(exponent))
+if(exponent<0):
+   res=1/res
+print(res)
